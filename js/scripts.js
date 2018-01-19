c745508c732b633f1b0d3c3af9edeeb1b4cd6e8f
Frank Ngo <frankngomusic@gmail.com>
2018-01-19 11:08:01 -0800

    add inital cases

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,31 @@
-
-
+function numberBot(number){
+  if(number%3 === 0){
+    return result = 1;
+  }
+  for (var i = 0; i < number.length; i ++) {
+    if(number[i].match(/0/)){
+      alert("Beep");
+    }
+    if(number[i].match(/1/)){
+      alert("Boop");
+    }
+  }
+}
 
 
 $(document).ready(function() {
   $("#number-form").submit(function(event){
     event.preventDefault();
     var number = parseInt($("#number").val());
-    var result = numberBot(number);
+    var newArray = [];
+    numberBot(number);
 
     $(".number").text(number);
 
-
+    if (result === 1) {
+      alert("I'm sorry, Dave. I'm afraid I can't do that.");
+    } else {
+      $(".not").text("");
+    }
   });
 });
