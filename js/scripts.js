c68415d2685476a8ac419bfa23fce7f0b5db5c83
Frank Ngo <frankngomusic@gmail.com>
2018-01-19 13:17:46 -0800

    fix all cases for all numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,31 @@
-function numberBot(number) {
-  if (number % 3 === 0) {
-    return result = 1;
-  } else if (number % 3 != 0) {
-    var number2 = ("" + number).split("").sort();
-    var newArray = [];
-    if (number2.includes("0")) {
-      for (var i = 0; i < number2.length; i++) {
-        newArray.push("Beep");
-      }
-      return alert(newArray);
-    } else if (number2.includes("1")) {
-      for (var i = 0; i < number2.length; i++) {
-        newArray.push("Boop");
-      }
-      return alert(newArray);
-    } else {
-      return number2;
-      alert(number2);
-    }
-  }
-}
-
-
 $(document).ready(function() {
-  $("#number-form").submit(function(event){
+  $("#number-form").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("#number").val());
-    var newArray =[];
-    numberBot(number);
-    $(".number").text(number);
 
+    if (number % 3 === 0) {
+      var result = 1
+    }
+    if (number % 3 != 0) {
+      var number2 = ("" + number).split("").sort();
+      var newArray = [];
+      if (number2.includes("0")) {
+        for (var i = 0; i < number2.length; i++) {
+          newArray.push("Beep");
+        }
+      } else if (number2.includes("1")) {
+        for (var i = 0; i < number2.length; i++) {
+          newArray.push("Boop");
+        }
+      }
+    }
+
+    
+    $(".number").text(number);
     if (result === 1) {
       $(".number").text("I'm sorry, Dave. I'm afraid I can't do that.");
     } else {
-      $(".number").text("boop");
+      $(".number").text(newArray);
     }
   });
 });
