164c98ebc57b624441eef70e197f583ed79b8200
Frank Ngo <frankngomusic@gmail.com>
2018-01-19 12:14:23 -0800

    add all cases for all numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
-function numberBot(number){
-  if(number%3 === 0){
+function numberBot(number) {
+  if (number % 3 === 0) {
     return result = 1;
-  }
-  for (var i = 0; i < number.length; i ++) {
-    if(number[i].match(/0/)){
-      alert("Beep");
-    }
-    if(number[i].match(/1/)){
-      alert("Boop");
+  } else if (number % 3 != 0) {
+    var number2 = ("" + number).split("").sort();
+    var newArray = [];
+    if (number2.includes("0")) {
+      for (var i = 0; i < number2.length; i++) {
+        newArray.push("Beep");
+      }
+      return alert(newArray);
+    } else if (number2.includes("1")) {
+      for (var i = 0; i < number2.length; i++) {
+        newArray.push("Boop");
+      }
+      return alert(newArray);
+    } else {
+      return number2;
+      alert(number2);
     }
   }
 }
@@ -17,15 +26,14 @@ $(document).ready(function() {
   $("#number-form").submit(function(event){
     event.preventDefault();
     var number = parseInt($("#number").val());
-    var newArray = [];
+    var newArray =[];
     numberBot(number);
-
     $(".number").text(number);
 
     if (result === 1) {
-      alert("I'm sorry, Dave. I'm afraid I can't do that.");
+      $(".number").text("I'm sorry, Dave. I'm afraid I can't do that.");
     } else {
-      $(".not").text("");
+      $(".number").text("boop");
     }
   });
 });
