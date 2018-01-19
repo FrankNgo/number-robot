e7c1426b65ebdb08696d5c7abbe6967b131e6324
Frank Ngo <frankngomusic@gmail.com>
2018-01-19 15:01:09 -0800

    add styles and error handling

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,35 @@
 $(document).ready(function() {
-  $("#number-form").submit(function(event) {
+  $("#numberForm").submit(function(event) {
     event.preventDefault();
     var number = parseInt($("#number").val());
 
     if (number % 3 === 0) {
-      var result = 1
+      var result = 1;
     }
     if (number % 3 != 0) {
-      var number2 = ("" + number).split("").sort();
+      var newNumber = ("" + number).split("").sort();
       var newArray = [];
-      if (number2.includes("0")) {
-        for (var i = 0; i < number2.length; i++) {
-          newArray.push("Beep");
+      if (newNumber.includes("0")) {
+        for (var i = 0; i < newNumber.length; i++) {
+          newArray.push(" Beep");
         }
-      } else if (number2.includes("1")) {
-        for (var i = 0; i < number2.length; i++) {
-          newArray.push("Boop");
+      } else if (newNumber.includes("1")) {
+        for (var i = 0; i < newNumber.length; i++) {
+          newArray.push(" Boop");
+        }
+      }else if (newNumber.includes("2") || newNumber.includes("3") || newNumber.includes("4") || newNumber.includes("5")
+             || newNumber.includes("6") || newNumber.includes("7") || newNumber.includes("8") || newNumber.includes("9")){
+        for (var i = 0; i < newNumber.length; i++) {
+          newArray.push(newNumber[i] + " ");
         }
       }
     }
 
-    
     $(".number").text(number);
     if (result === 1) {
       $(".number").text("I'm sorry, Dave. I'm afraid I can't do that.");
     } else {
-      $(".number").text(newArray);
+      $(".number").text(newArray.join(""));
     }
   });
 });
